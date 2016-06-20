f2fd02b11ce87df5458e2c2b739857b02ef01664
ninini976 <yangtony@umich.edu>
2016-06-20 02:04:12 -0700

    working on EM for large sequence

--- main.py
+++ main.py
@@ -44,55 +44,55 @@ for i in range(0,101):
 	print best_pstable
 
 
-# txt = np.loadtxt('data')
-# observations = []
-# pt_observation = []
-# for i in range (0,5692):
-# 	if i < 5691 and txt[i+1][0] == 1:
-# 		if txt[i][2] <= 6:
-# 			pt_observation.append("-")
-# 		else:
-# 			pt_observation.append("+")
-# 		observations.append(pt_observation)
-# 		pt_observation = []
-# 	else: 
-# 		if i < 5691 and txt[i+1][0] != 1:
-# 			if txt[i][2] <= 6:
-# 				pt_observation.append("-")
-# 			else:
-# 				pt_observation.append("+")
-# 		else:
-# 			if txt[i][2] <= 6:
-# 				pt_observation.append("-")
-# 			else:
-# 				pt_observation.append("+")
-# 			observations.append(pt_observation)
+txt = np.loadtxt('data')
+observations = []
+pt_observation = []
+for i in range (0,5692):
+	if i < 5691 and txt[i+1][0] == 1:
+		if txt[i][2] <= 6:
+			pt_observation.append("-")
+		else:
+			pt_observation.append("+")
+		observations.append(pt_observation)
+		pt_observation = []
+	else: 
+		if i < 5691 and txt[i+1][0] != 1:
+			if txt[i][2] <= 6:
+				pt_observation.append("-")
+			else:
+				pt_observation.append("+")
+		else:
+			if txt[i][2] <= 6:
+				pt_observation.append("-")
+			else:
+				pt_observation.append("+")
+			observations.append(pt_observation)
 
-# ps_array = []
+ps_array = []
 
-# print len(observations)
+print len(observations)
 
-# for ob in observations:
-# 	if len(ob) == 1:
-# 		continue
-# 	print ob
-# 	if check_all_neg(ob):
-# 		# print "The pstable that can best fit this observation: 1.0"
-# 		if len(ob) > 9:
-# 			ps_array.append(1)
-# 			print 1.0
-# 		else:
-# 			process_neg(ob)
-# 		continue 
-# 	max = 0
-# 	best_pstable = 0
-# 	for i in range(0,101):
-# 		pstable = float(i)/100
-# 		hmm1.set_transition_matrix([[pstable,1-pstable],[0,1]])
-# 		p = hmm1.observation_possibility(ob)
-# 		if p > max:
-# 			max = p
-# 			best_pstable = pstable
-#  	ps_array.append(best_pstable)
-#  	print best_pstable
+for ob in observations:
+	if len(ob) == 1:
+		continue
+	print ob
+	if check_all_neg(ob):
+		# print "The pstable that can best fit this observation: 1.0"
+		if len(ob) > 9:
+			ps_array.append(1)
+			print 1.0
+		else:
+			process_neg(ob)
+		continue 
+	max = 0
+	best_pstable = 0
+	for i in range(0,101):
+		pstable = float(i)/100
+		hmm1.set_transition_matrix([[pstable,1-pstable],[0,1]])
+		p = hmm1.observation_possibility(ob)
+		if p > max:
+			max = p
+			best_pstable = pstable
+ 	ps_array.append(best_pstable)
+ 	print best_pstable
 
--- test.py
+++ test.py
@@ -1,17 +1,150 @@
 import hmm
+from io import StringIO
+import numpy as np
 
 hmm1 = hmm.HMM(2)
 hmm1.set_obervation(["-","+"])
 hmm1.set_transition_matrix([[0.8,0.2],[0,1]])
-hmm1.set_observation_matrix([[0.894,0.106],[0.375,0.625]])
+hmm1.set_observation_matrix([[0.90,0.10],[0.375,0.625]])
 hmm1.set_pi([0.7488,0.2512])
 
+txt = []
+file = open('data')
+for line in file:
+	txt.append(line.split())
 
-ob_seq = ['-','-','-','-']
-p1 = hmm1.gama(1,0,ob_seq)
-p2 = hmm1.xi(1,0,0,ob_seq)
-p3 = hmm1.xi(1,0,1,ob_seq)
 
+
+
+observations = []
+pt_observation = []
+for i in range (0,5692):
+	if i < 5691 and int(txt[i+1][0]) == 1:
+		if int(txt[i][2]) <= 6:
+			pt_observation.append("-")
+		else:
+			pt_observation.append("+")
+		observations.append(pt_observation)
+		pt_observation = []
+	else: 
+		if i < 5691 and int(txt[i+1][0]) != 1:
+			if int(txt[i][2]) <= 6:
+				pt_observation.append("-")
+			else:
+				pt_observation.append("+")
+		else:
+			if int(txt[i][2]) <= 6:
+				pt_observation.append("-")
+			else:
+				pt_observation.append("+")
+			observations.append(pt_observation)
+
+ps_array = []
+print observations
+observations = [elem for elem in observations if len(elem)>1]
+for ob in observations:
+	print str(ob)+','
+
+print len(observations)
+
+observations = [['-', '-', '+'],
+['-', '-'],
+['-', '-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-', '-'],
+['-', '+'],
+['-', '+'],
+['-', '-', '-'],
+['-', '-', '-', '-'],
+['-', '-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-', '-', '-'],
+['-', '-'],
+['-', '-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '+', '+'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['+', '-'],
+['-', '-'],
+['-', '-', '-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-', '-'],
+['-', '-', '+'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-', '-', '-'],
+['-', '-', '-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '+'],
+['-', '+'],
+['-', '+'],
+['-', '+'],
+['-', '-'],
+['-', '-'],
+['-', '+'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-', '-'],
+['-', '-', '-'],
+['-', '+'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '+'],
+['-', '+'],
+['-', '-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '+'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '+'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '+'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-'],
+['-', '-']
+]
 # following part is em for a00
 a00 = 0.6
 out = 0.6
@@ -20,12 +153,17 @@ hmm1.set_transition_matrix([[a00,1-a00],[0,1]])
 while True:
 	a00 = out
 	hmm1.set_transition_matrix([[a00, 1-a00],[0,1]])
-	xi_sum = 0
-	gama_sum = 0
-	for i in range(len(ob_seq)-1):
-		xi_sum = xi_sum + hmm1.xi(i,0,0,ob_seq)
-		gama_sum = gama_sum + hmm1.gama(i,0,ob_seq)
-	out = xi_sum/gama_sum
+	divisor = 0
+	dividend = 0
+	for ob in observations:
+		xi_sum = 0
+		gama_sum = 0
+		for t in range(len(ob)-1):
+			xi_sum = xi_sum + hmm1.xi(t,0,0,ob)
+			gama_sum = gama_sum + hmm1.gama(t,0,ob)
+		dividend = dividend + xi_sum
+		divisor = divisor + gama_sum
+	out = dividend/divisor
 	print out
 	if abs(out - a00) < error_tolerence:
 		break
