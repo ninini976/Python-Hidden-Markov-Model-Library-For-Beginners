0ecbdddc0964a02410f8bc5608659499ee826932
ninini976 <yangtony@umich.edu>
2016-06-23 20:36:09 -0700

    6/23

--- test.py
+++ test.py
@@ -1,57 +1,56 @@
 import hmm
 from io import StringIO
 import numpy as np
+import math
 
 hmm1 = hmm.HMM(2)
 hmm1.set_obervation(["-","+"])
 hmm1.set_transition_matrix([[0.8,0.2],[0,1]])
-hmm1.set_observation_matrix([[0.90,0.10],[0.375,0.625]])
+hmm1.set_observation_matrix([[1.00,0.00],[0.375,0.625]])
 hmm1.set_pi([0.7488,0.2512])
 
 #........The following part is the code for reading data form file "data"
 
 
-# txt = []
-# file = open('data')
-# for line in file:
-# 	txt.append(line.split())
+txt = []
+file = open('data')
+for line in file:
+	txt.append(line.split())
 
 
 
 
-# observations = []
-# pt_observation = []
-# for i in range (0,5692):
-# 	if i < 5691 and int(txt[i+1][0]) == 1:
-# 		if int(txt[i][2]) <= 6:
-# 			pt_observation.append("-")
-# 		else:
-# 			pt_observation.append("+")
-# 		observations.append(pt_observation)
-# 		pt_observation = []
-# 	else: 
-# 		if i < 5691 and int(txt[i+1][0]) != 1:
-# 			if int(txt[i][2]) <= 6:
-# 				pt_observation.append("-")
-# 			else:
-# 				pt_observation.append("+")
-# 		else:
-# 			if int(txt[i][2]) <= 6:
-# 				pt_observation.append("-")
-# 			else:
-# 				pt_observation.append("+")
-# 			observations.append(pt_observation)
+observations = []
+pt_observation = []
+for i in range (0,5692):
+	if i < 5691 and int(txt[i+1][0]) == 1:
+		if int(txt[i][2]) <= 6:
+			pt_observation.append("-")
+		else:
+			pt_observation.append("+")
+		observations.append(pt_observation)
+		pt_observation = []
+	else: 
+		if i < 5691 and int(txt[i+1][0]) != 1:
+			if int(txt[i][2]) <= 6:
+				pt_observation.append("-")
+			else:
+				pt_observation.append("+")
+		else:
+			if int(txt[i][2]) <= 6:
+				pt_observation.append("-")
+			else:
+				pt_observation.append("+")
+			observations.append(pt_observation)
 
-# ps_array = []
-# print observations
-# observations = [elem for elem in observations if len(elem)>1]
-# for ob in observations:
-# 	print str(ob)+','
+ps_array = []
+print observations
+observations = [elem for elem in observations if len(elem)>1]
+for ob in observations:
+	print str(ob)+','
 
-# print len(observations)
+print len(observations)
 
-# here is an example of obervations
-observations = [['-','-','-'],['-','+'],['-','-','-','+'],['-','-','-','-','-','+']]
 
 
 
@@ -89,15 +88,16 @@ print out
 
 
 # This is the grid search on maximizing the product of P for each sequence
-Max_p = 0
-Max = 0
-for i in range(1,101):
-	a = float(i)/100
-	p = 1
-	for ob in observations:
-		hmm1.set_transition_matrix([[a,1-a],[0,1]])
-		p = p * hmm1.observation_possibility(ob)
-	if p > Max_p:
-		Max = a
-		Max_p = p
-print Max
+# Max_p_log = -1000000000000000000000000000000000000
+# Max = 0
+# for i in range(1,101):
+# 	a = float(i)/100
+# 	log = 0
+# 	for ob in observations:
+# 		hmm1.set_transition_matrix([[a,1-a],[0,1]])
+# 		log = log + math.log(hmm1.observation_possibility(ob))
+# 	print log	
+# 	if log > Max_p_log:
+# 		Max = a
+# 		Max_p_log = log
+# print Max
