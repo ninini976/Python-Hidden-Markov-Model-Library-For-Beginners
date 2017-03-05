321e7e3bafa57dc48cc324e55bc3ad6952c33c43
ninini976 <yangtony@umich.edu>
2017-03-04 19:41:20 -0800

    simulation data

--- Validation/generate_simulation_data.py
+++ Validation/generate_simulation_data.py
@@ -1,58 +1,44 @@
 import hmm
-from io import StringIO
 import numpy as np
-import math
-import sys
 
-hmm1 = hmm.HMM(2)
-hmm1.set_obervation(["-","+"])
-hmm1.set_transition_matrix([[0.9,0.1],[0,1]])
-hmm1.set_observation_matrix([[0.8,0.2],[0.3,0.7]])
-hmm1.set_pi([0.85,0.15])
+# initialize a hmm model using the parameter we get from EM alg.
+hmm = hmm.HMM(2)
+hmm.set_obervation(["-","+"])
+hmm.set_transition_matrix([[0.9603,0.0397],[0,1]])
+hmm.set_observation_matrix([[0.9862,0.0138],[0.3897,0.6103]])
+hmm.set_pi([0.8664,0.1336])
 
-#........The following part is the code for reading data form file "data"
+obs = []
+for i in range(1375):
+	ob = hmm.gen_ob_seq(13)
+	obs.append(ob)
 
-txt = []
-file = open('data')
+drop = [0.2598,0.2450,0.2732,0.2898,0.3415,0.3068,0.3805,0.2687,0.2609,0.5484,0.7692,0.5000,1]
 
-
-for line in file: # This for loop splite each line in the data file to 3 numbers and saved in the list "txt"
-	txt.append(line.split())
-
-observations = []
-pt_observation = []
-for i in range (0,5692): # 5692 is the total number of records
-	if i < 5691 and int(txt[i+1][0]) == 1:
-		if int(txt[i][2]) <= 6:
-			pt_observation.append("-")
-		else:
-			pt_observation.append("+")
-		observations.append(pt_observation)
-		pt_observation = []
-	else: 
-		if i < 5691 and int(txt[i+1][0]) != 1:
-			if int(txt[i][2]) <= 6:
-				pt_observation.append("-")
+simulated_data = []
+new_obs = list(obs)
+# i = 1, ... , 12
+for i in range(13):
+	old_obs = list(new_obs)
+	new_obs = []
+	for ob in old_obs:
+		if ob[i] == "+":
+			if np.random.uniform() < 0.943:
+				simulated_data.append(ob[:i+1])
+			elif np.random.uniform() < 0.55:
+				simulated_data.append(ob[:i+2])
 			else:
-				pt_observation.append("+")
-		else:
-			if int(txt[i][2]) <= 6:
-				pt_observation.append("-")
+				simulated_data.append(ob[:i+3])
+		if ob[:i+1] == ['-' for i in range(i+1)]:
+			if np.random.uniform() < drop[i]:
+				simulated_data.append(['-' for i in range(i+1)])
 			else:
-				pt_observation.append("+")
-			observations.append(pt_observation)
+				new_obs.append(ob)
 
+for ob in simulated_data:
+	print(ob)
 
-# This is a filter that only filter out those seqences with only one record
-observations = [elem for elem in observations if len(elem)>1]
-
-# In each sequence, remove the diagnosis stage data and print
-for ob in observations:
-	ob.remove(ob[0])
-	#print ob
-
-# Print the number of observation seqences in total
-#print len(observations)
+observations = list(simulated_data)
 
 print("Number of all biopsies taken: %d" % sum([len(ob) for ob in observations]))
 print("Number of positive biopsy outcome: %d" % sum([ob.count('+') for ob in observations]))
@@ -90,4 +76,4 @@ for i in range(12):
 				total += 1
 				if (len(ob) == len(prefix)):
 					drop += 1
-	print("Drop out rate after stage %d : %.4f" % ((i+1), (float(drop)/total)))
+	print("Drop out rate after stage %d : %.4f" % ((i+1), (float(drop)/total if total != 0 else 0)))
